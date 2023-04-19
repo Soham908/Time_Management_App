7ce0d5f5497f97f3810e4bddf9d7d72d114af6e3
Soham908 <sohampalekar908@gmail.com>
2023-04-19 12:38:01 +0530

    fully implemented stopwatch service

--- app/src/main/java/com/example/timemanagementapp/ui/StopWatchFragment.kt
+++ app/src/main/java/com/example/timemanagementapp/ui/StopWatchFragment.kt
@@ -3,26 +3,34 @@ package com.example.timemanagementapp.ui
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.preference.PreferenceManager
 import com.example.timemanagementapp.R
 import com.example.timemanagementapp.databinding.FragmentStopWatchBinding
 import com.example.timemanagementapp.ui.services.StopWatchService
+import com.example.timemanagementapp.ui.services.StopWatchViewModel
 
 class StopWatchFragment : Fragment() {
     private lateinit var binding: FragmentStopWatchBinding
-    private var timeInSeconds: Long = 0
-    private var running = false
-    private lateinit var handler: Handler
-    private lateinit var runnable: Runnable
+    lateinit var service2: StopWatchService
+    private lateinit var service: StopWatchService
+    private var running = StopWatchService.runningSavedState
+    private var isPause = StopWatchService.isPauseSavedState
+    // takes the foreground variable because when the app is closed and opened again at that time the apps var are destroyed
+    // and it will start another service because values are reset
+    // so when app destroys pass the state of the var in from fragment to service so that state is saved in the service
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val currentTheme = if (isDarkThemeEnabled()) R.style.AppTheme_Dark else R.style.AppTheme_Dark
         val themedInflater = inflater.cloneInContext(ContextThemeWrapper(activity, currentTheme))
+        service = StopWatchService()
         return themedInflater.inflate(R.layout.fragment_stop_watch, container, false)
     }
 
@@ -30,7 +38,6 @@ class StopWatchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        val view = inflater.inflate(R.layout.fragment_stop_watch, container, false)
         binding = FragmentStopWatchBinding.bind(view)
 
         val timer = binding.timerTextView
@@ -39,54 +46,42 @@ class StopWatchFragment : Fragment() {
         val reset = binding.resetButton
 
 
-        // handler and runnable is used to handle all the updates for the UI
-        // it always keeps running in the background until it is stopped
-        // chronometer also uses handler to update its UI
-        // to handle stopwatch and countdown use handler and runnable it acts on the main thread and updates regularly without needed to be called
-        handler = Handler()
-        // handler deprecated change to executor afterwards
-        runnable = object : Runnable {
-            override fun run() {
-                val hours = timeInSeconds / 3600
-                val minutes = timeInSeconds % 3600 / 60
-                val seconds = timeInSeconds % 60
-                val timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds)
-                timer.text = timeString
-                if (running) {
-                    timeInSeconds++
-                }
-                handler.postDelayed(this, 1000)
-            }
-        }
         // when it starts it takes a little time to start counting
         start.setOnClickListener {
+            if (!running && !isPause){ requireActivity().startService(Intent(context, StopWatchService::class.java)) }
+            else if (!running && isPause){ service.resumeStopWatch() }
             running = true
-            requireActivity().startService(Intent(context, StopWatchService::class.java))
         }
         stop.setOnClickListener {
+            if (running){ service.pauseStopWatch() }
+            isPause = true
             running = false
-            requireActivity().stopService(Intent(context, StopWatchService::class.java))
         }
         reset.setOnClickListener {
             running = false
-            timeInSeconds = 0
+            requireActivity().stopService(Intent(context, StopWatchService::class.java))
             timer.text = getString(R.string.startTime)
         }
 
+        StopWatchService.num.observe(viewLifecycleOwner, Observer {elapsedTime ->
+            Log.d("dataFirebase", elapsedTime.toString())
+            val hours = elapsedTime / 3600
+            val minutes = (elapsedTime % 3600) / 60
+            val secs = elapsedTime % 60
+            timer.text = String.format("%02d:%02d:%02d", hours, minutes, secs)
+        })
+
     }
 
-    override fun onResume() {
-        super.onResume()
-        handler.postDelayed(runnable, 1000)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        handler.removeCallbacks(runnable)
-    }
 
     private fun isDarkThemeEnabled(): Boolean {
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
         return sharedPreferences.getBoolean("dark_theme_enabled", false)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        StopWatchService.isPauseSavedState = true
+        StopWatchService.runningSavedState = true
     }
 }
--- app/src/main/java/com/example/timemanagementapp/ui/services/StopWatchService.kt
+++ app/src/main/java/com/example/timemanagementapp/ui/services/StopWatchService.kt
@@ -1,26 +1,14 @@
 package com.example.timemanagementapp.ui.services
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.app.Service
+import android.app.*
 import android.content.Context
 import android.content.Intent
-import android.os.Build
-import android.os.Handler
 import android.os.IBinder
-import android.os.Looper
-import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModelProvider
 import com.example.timemanagementapp.MainActivity
 import com.example.timemanagementapp.R
-import com.example.timemanagementapp.ui.StopWatchFragment
 import kotlinx.coroutines.*
-import java.util.Timer
-import java.util.concurrent.TimeUnit
 
 
 class StopWatchService : Service() {
@@ -33,30 +21,32 @@ class StopWatchService : Service() {
     companion object {
         private const val NOTIFICATION_CHANNEL_ID = "timer_channel"
         private const val NOTIFICATION_ID = 1
+        var num = MutableLiveData<Long>()
+        // Just be aware that using global variables can lead to potential issues such as data inconsistency and memory leaks
+        var isPause = false
+        var runningSavedState = false
+        var isPauseSavedState = isPause
     }
+
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
-    lateinit var viewModel: StopWatchViewModel
-
-    override fun onCreate() {
-        super.onCreate()
-        viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(application).create(StopWatchViewModel::class.java)
-
-    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        num.postValue(0L)
         startForeground(NOTIFICATION_ID, buildNotification())
 
         timerJob = CoroutineScope(Dispatchers.Main).launch {
             var seconds = 0L
             while (true) {
                 delay(1000)
-                seconds++
-                updateNotification(seconds)
-                viewModel.getElapsedTime(seconds)
+                if (!isPause){
+                    seconds++
+                    updateNotification(seconds)
+                    num.postValue(seconds)
+                }
             }
         }
 
@@ -95,76 +85,19 @@ class StopWatchService : Service() {
         notificationManager.notify(NOTIFICATION_ID, notification)
     }
 
+    fun pauseStopWatch(){
+        isPause = true
+    }
+    fun resumeStopWatch(){
+        isPause = false
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         timerJob?.cancel()
-//        stopForeground(true)
-        stopForeground(Service.STOP_FOREGROUND_REMOVE)
+        stopForeground(STOP_FOREGROUND_REMOVE)
     }
 }
 
 
 
-
-//class StopWatchService() : Service() {
-//    private var elapsedTime: Long = 0
-//    private lateinit var handler: Handler
-//     lateinit var viewModel: StopWatchViewModel
-//
-//    override fun onBind(p0: Intent?): IBinder? = null
-//
-//    override fun onCreate() {
-//        super.onCreate()
-//        createNotification()
-//        viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(application).create(StopWatchViewModel::class.java)
-//    }
-//
-//    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-//        handler = Handler(Looper.getMainLooper())
-//
-//        Thread {
-//            while (true) {
-//                elapsedTime += 1000
-//                handler.post {
-//                    showNotification()
-//                    viewModel.getElapsedTime(elapsedTime)
-//                }
-//                Thread.sleep(1000)
-//            }
-//        }.start()
-//
-//        return START_STICKY
-//    }
-//
-//    private fun showNotification() {
-//        val notificationIntent = Intent(this, MainActivity::class.java)
-//        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
-//
-//        val hours = TimeUnit.MILLISECONDS.toHours(elapsedTime)
-//        val minutes = TimeUnit.MILLISECONDS.toMinutes(elapsedTime) % 60
-//        val seconds = TimeUnit.MILLISECONDS.toSeconds(elapsedTime) % 60
-//
-//        val notification = Notification.Builder(this, "notiID")
-//            .setContentTitle("Stopwatch")
-//            .setContentText(String.format("%02d:%02d:%02d", hours, minutes, seconds))
-//            .setSmallIcon(R.drawable.bottom_nav_homepage)
-//            .setContentIntent(pendingIntent)
-//            .setOngoing(true)
-//            .build()
-//
-//        val manager = getSystemService(NotificationManager::class.java)
-//        manager.notify(123, notification) // use the same notification ID every time to update the existing notification
-//
-//        val timer = Timer()
-//        timer.scheduleAtFixedRate()
-//    }
-//
-//
-//    private fun createNotification() {
-//        val showChannel = NotificationChannel("notiID", "Timer Service", NotificationManager.IMPORTANCE_LOW)
-//        val manager = getSystemService(NotificationManager::class.java)
-//        manager.createNotificationChannel(showChannel)
-//    }
-//
-//
-//}
