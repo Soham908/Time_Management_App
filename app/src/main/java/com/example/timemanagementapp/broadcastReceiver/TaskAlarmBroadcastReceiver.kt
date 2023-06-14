26dc1c8d3c2f41d4c25f483d0ab88bbe097de40e
Soham908 <sohampalekar908@gmail.com>
2023-06-14 19:02:13 +0530

    added alarm sound and set limit of 1 min to task alarm

--- app/src/main/java/com/example/timemanagementapp/broadcastReceiver/TaskAlarmBroadcastReceiver.kt
+++ app/src/main/java/com/example/timemanagementapp/broadcastReceiver/TaskAlarmBroadcastReceiver.kt
@@ -7,16 +7,21 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.AudioAttributes
+import android.os.Handler
+import android.os.Looper
+import android.provider.Settings
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.example.timemanagementapp.MainActivity
 import com.example.timemanagementapp.R
+import com.example.timemanagementapp.services.TaskAlarmScheduler
 
-class TaskAlarmBrodcastReciever: BroadcastReceiver() {
+class TaskAlarmBroadcastReceiver: BroadcastReceiver() {
 
     private lateinit var notification: NotificationCompat.Builder
-    lateinit var notificationManagerCompat: NotificationManagerCompat
+    private lateinit var notificationManagerCompat: NotificationManagerCompat
     private val CHANNEL_ID = "TaskAlarm"
     private lateinit var taskSubject: String
 
@@ -32,8 +37,21 @@ class TaskAlarmBrodcastReciever: BroadcastReceiver() {
     }
 
     private fun buildNotification(context: Context): NotificationCompat.Builder {
-        val channel = NotificationChannel(CHANNEL_ID, "Task Alarm Scheduled", NotificationManager.IMPORTANCE_HIGH)
+        val audioAlarm = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_ALARM)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+
+        val channel = NotificationChannel(CHANNEL_ID, "Task Alarm Scheduled", NotificationManager.IMPORTANCE_HIGH).apply {
+            setSound(Settings.System.DEFAULT_ALARM_ALERT_URI, audioAlarm)
+        }
         notificationManagerCompat.createNotificationChannel(channel)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            notificationManagerCompat.cancel(taskSubject.hashCode())
+//            val time = System.currentTimeMillis() + 1 * 60 * 1000
+//            TaskAlarmScheduler(context).scheduleAlarm(time, taskSubject)
+        }, 60000)
 
         val intent = Intent(context, MainActivity::class.java).apply {
             putExtra("Fragment", "taskFragmentIntent")
@@ -45,6 +63,7 @@ class TaskAlarmBrodcastReciever: BroadcastReceiver() {
             .setContentText("complete $taskSubject")
             .setContentIntent(pendingIntent)
             .setSmallIcon(R.drawable.alarm_icon)
+            .setCategory(NotificationCompat.CATEGORY_ALARM)
 
     }
 }
--- app/src/main/java/com/example/timemanagementapp/services/TaskAlarmScheduler.kt
+++ app/src/main/java/com/example/timemanagementapp/services/TaskAlarmScheduler.kt
@@ -7,7 +7,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
-import com.example.timemanagementapp.broadcastReceiver.TaskAlarmBrodcastReciever
+import com.example.timemanagementapp.broadcastReceiver.TaskAlarmBroadcastReceiver
 import java.util.concurrent.TimeUnit
 
 class TaskAlarmScheduler(val context: Context) {
@@ -16,7 +16,7 @@ class TaskAlarmScheduler(val context: Context) {
     
     @SuppressLint("MissingPermission")
     fun scheduleAlarm(time: Long, taskSubject: String){
-        val intent = Intent(context, TaskAlarmBrodcastReciever::class.java).apply {
+        val intent = Intent(context, TaskAlarmBroadcastReceiver::class.java).apply {
             putExtra("extra_message", taskSubject)
         }
         val pendingIntent = PendingIntent.getBroadcast(context, taskSubject.hashCode(), intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
